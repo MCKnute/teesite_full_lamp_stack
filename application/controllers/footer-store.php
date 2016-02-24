a81061597b5d96f2a06bcfe999b14e425fbdc32e
Mollie Knute <mcknute@gmail.com>
2016-02-24 12:58:18 -0800

    Added front-end main and product pages, with Bootstrap framework

---
+++ application/controllers/footer-store.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+	</div>
+  <footer>
+    <p>© 2015 Mollie Knute, David Macias, and Pete Kang</p>
+  </footer>
+</body>
+</html>
