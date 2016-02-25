871593463d72f8e120fd18c7919a7d9e1d5aaf2a
David Macias <davymac@gmail.com>
2016-02-24 23:56:25 -0800

    forgot to add the about_us.php file

---
+++ application/views/about_us.php
@@ -0,0 +1,8 @@
+<div class="container container-main">
+<img class="center-block" src="/assets/img/white.png" alt=''>
+<div id="googlemap" class="col-md-4">
+<p>Le Google Goes Here</p>
+</div>
+<div id="nonsense" class="col-md-8">
+<p>KMK Tees is the industry leader of distributed solutions. Imagine a combination of ASP and HTTP. The social-network-based fractal e-business re-sizing factor can be summed up in one word: bricks-and-clicks, back-end. We invariably grow next-generation social networks. That is an amazing achievement when you consider this fiduciary term's financial state of things! Think leading-edge. Think out-of-the-box. Think cross-platform. But don't think all three at the same time. Imagine a combination of XSLT and XML. Without well-chosen web services, eyeballs are forced to become e-business. We think that most distributed web portals use far too much XMLHttpRequest, and not enough XSLT. Imagine a combination of Flash and ASP. We frequently unleash blog-based R&D. That is a remarkable achievement taking into account this year's market conditions! We understand that it is better to innovate intuitively than to transition intuitively.</p>
+</div>
