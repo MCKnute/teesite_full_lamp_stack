19a38c104c4a35743f690096a2a4cdf00efa9437
David Macias <davymac@gmail.com>
2016-02-24 22:38:58 -0800

    fiddle with footer debug and started about us... this is really just because i forgot to switch over to a new branch, but luckily nothing major was done yet.

--- application/views/footer-store.php
+++ application/views/footer-store.php
@@ -5,9 +5,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</div>
   <footer>
     <p>© 2016 Mollie Knute, David Macias, and Pete Kang</p>
+    <hr>
     <h5>debug sandbox:</h5>
-    <p>session data:</p>
+    <p><b>$_SESSION</b> data:</p>
     	<? var_dump($_SESSION) ;?>
+    	<hr>
+    <p><b>"$this->session->userdata()"</b> data:</p>	
+    	<? var_dump($this->session->userdata()) ;?>
+    	<hr>
     <p>cart data:</p>
     <p>items in cart: <?=$this->cart->total()?></p>
   </footer>
