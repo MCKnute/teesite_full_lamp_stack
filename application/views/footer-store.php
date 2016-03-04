0656026fa40bd3cd265c2811ef22dbbe0d36a405
Mollie Knute <mcknute@gmail.com>
2016-03-03 16:56:19 -0800

    More stylizations with footer and store

--- application/views/footer-store.php
+++ application/views/footer-store.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</div>
   <footer>
     <p>© 2016 Mollie Knute, David Macias, and Peter Kang</p>
-    <hr>
+<!--     <hr>
     <h5>debug sandbox:</h5>
     <p><strong>$_POST</strong> data:</p>
         <? var_dump($_POST) ;?>
@@ -15,7 +15,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     	<hr />
     <p>cart data:</p>
     <p>Number of items in cart: <?=$this->cart->total()?></p>
-    <p>Products in cart: <br /><?php var_dump($this->cart->contents()); ?></p>
+    <p>Products in cart: <br /><?php var_dump($this->cart->contents()); ?></p> -->
 
     <!-- <a href="/Users/logout">Log Out</a> -->
     <!-- <a href="/Carts/remove_items/all">Empty Cart</a> -->
